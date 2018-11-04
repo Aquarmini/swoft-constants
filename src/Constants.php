a3133606fa16038372df31a54bb711dfb647b494
李铭昕 <715557344@qq.com>
2018-11-04 19:34:58 +0800

    完善第一版本

--- src/Constants.php
+++ src/Constants.php
@@ -9,6 +9,43 @@
  */
 namespace Swoftx\Constants;
 
+use limx\Support\Str;
+use Swoftx\Constants\Adapters\ReflectionAdapter;
+use Swoftx\Constants\Exceptions\ConstantsException;
+
+/**
+ * Class Constants
+ * @package Swoftx\Constants
+ */
 abstract class Constants
 {
+    public static $mapping;
+
+    public static function __callStatic($name, $arguments)
+    {
+        if (!Str::startsWith($name, 'get')) {
+            throw new ConstantsException('The function is not defined!');
+        }
+
+        if (!isset($arguments) || count($arguments) === 0) {
+            throw new ConstantsException('The Code is required');
+        }
+
+        $code = $arguments[0];
+        $name = strtolower(substr($name, 3));
+
+        if (isset(static::$mapping[$name])) {
+            return isset(static::$mapping[$name]) ? static::$mapping[$name][$code] : '';
+        }
+
+        // 获取变量
+        $ref = new \ReflectionClass(static::class);
+        $classConstants = $ref->getReflectionConstants();
+
+        $adapter = new ReflectionAdapter(static::class);
+        $result = $adapter->getAnnotationsByName($name, $classConstants);
+
+        static::$mapping[$name] = $result;
+        return isset(static::$mapping[$name][$code]) ? static::$mapping[$name][$code] : '';
+    }
 }
---
+++ tests/Testing/Constants/ErrorCode.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace SwoftTest\Testing\Constants;
+
+use Swoftx\Constants\Constants;
+
+class ErrorCode extends Constants
+{
+    /**
+     * @Message('参数错误')
+     */
+    const PARAMS_INVALID = 500;
+}
